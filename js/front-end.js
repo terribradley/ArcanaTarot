ca28e4d8951eef6dbd3217010afd6741dff2e8e4
Terri Bradley <terribradley3@gmail.com>
2016-10-25 10:14:28 -0700

    initial commit

---
+++ js/front-end.js
@@ -0,0 +1,21 @@
+
+
+$(document).ready(function() {
+  $("#btn").click(function(){
+    var div = $("#line");
+    var button = $("#btn");
+    var bar = $(".bar");
+    var input = $(".text-box ");
+    var label = $(".text-box-label");
+    var html = $("html, body");
+    input.hide();
+    bar.hide();
+    label.hide();
+    div.show();
+    div.animate({height: '10px'}, 200).animate({width: '680px'}, 2000).animate({height: '450px'}, 1000);
+    // div.animate({width: '650px'}, 800);
+    // div.animate({rotateX: "90deg"}, "slow");
+    html.animate({scrollTop: $(".card-images").offset().top}, 2000);
+    button.animate({right: '-900px'}, 2000);
+    });
+});
---
+++ js/scripts.js
@@ -0,0 +1,181 @@
+var cardarray = [
+  {
+    image: "img/card_1.png",
+    reading: "1 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna."
+  },{
+    image: "img/card_2.png",
+    reading: "2 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna."
+  },{
+    image: "img/card_3.png",
+    reading: "3 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna."
+  },{
+    image: "img/card_4.png",
+    reading: "4 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna."
+  },{
+    image: "img/card_5.png",
+    reading: "5 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna."
+  },{
+    image: "img/card_6.png",
+    reading: "6 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna."
+  },{
+    image: "img/card_7.png",
+    reading: "7 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna."
+  },{
+    image: "img/card_8.png",
+    reading: "8 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna."
+  },{
+    image: "img/card_9.png",
+    reading: "9 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna."
+  },{
+    image: "img/card_10.png",
+    reading: "10 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna."
+  },{
+    image: "img/card_11.png",
+    reading: "11 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna."
+  },{
+    image: "img/card_12.png",
+    reading: "12 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna."
+  },{
+    image: "img/card_13.png",
+    reading: "13 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna."
+  },{
+    image: "img/card_14.png",
+    reading: "14 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna."
+  },{
+    image: "img/card_15.png",
+    reading: "15 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna."
+  },{
+    image: "img/card_16.png",
+    reading: "16 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna."
+  },{
+    image: "img/card_17.png",
+    reading: "17 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna."
+  },{
+    image: "img/card_18.png",
+    reading: "18 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna."
+  },{
+    image: "img/card_19.png",
+    reading: "19 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna."
+  },{
+    image: "img/card_20.png",
+    reading: "20 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna."
+  },{
+    image: "img/card_21.png",
+    reading: "21 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna."
+  },{
+    image: "img/card_22.png",
+    reading: "22 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna."
+  },{
+    image: "img/card_23.png",
+    reading: "23 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna."
+  },{
+    image: "img/card_24.png",
+    reading: "25 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna."
+  },{
+    image: "img/card_26.png",
+    reading: "26 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna."
+  },{
+    image: "img/card_27.png",
+    reading: "27 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna."
+  },{
+    image: "img/card_28.png",
+    reading: "28 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna."
+  },{
+    image: "img/card_29.png",
+    reading: "29 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna."
+  },{
+    image: "img/card_30.png",
+    reading: "30 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna."
+  },{
+    image: "img/card_31.png",
+    reading: "31 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna."
+  },{
+    image: "img/card_32.png",
+    reading: "32 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna."
+  },{
+    image: "img/card_33.png",
+    reading: "33 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna."
+  },{
+    image: "img/card_34.png",
+    reading: "34 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna."
+  },{
+    image: "img/card_35.png",
+    reading: "35 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna."
+  },{
+    image: "img/card_36.png",
+    reading: "37 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna."
+  },{
+    image: "img/card_38.png",
+    reading: "38 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna."
+  },{
+    image: "img/card_39.png",
+    reading: "39 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna."
+  },{
+    image: "img/card_40.png",
+    reading: "40 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna."
+  },{
+    image: "img/card_41.png",
+    reading: "41 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna."
+  },{
+    image: "img/card_42.png",
+    reading: "42 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna."
+  },{
+    image: "img/card_43.png",
+    reading: "43 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna."
+  },{
+    image: "img/card_44.png",
+    reading: "44 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna."
+  },{
+    image: "img/card_45.png",
+    reading: "45 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna."
+  },{
+    image: "img/card_46.png",
+    reading: "46 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna."
+  },{
+    image: "img/card_47.png",
+    reading: "47 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna."
+  },{
+    image: "img/card_48.png",
+    reading: "48 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna."
+  },{
+    image: "img/card_49.png",
+    reading: "49 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna."
+  },{
+    image: "img/card_50.png",
+    reading: "50 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna."
+  }
+];
+
+function shuffle(shufflearray) {
+  var i = shufflearray.length;
+  var j;
+  var temp;
+
+  while(i-- > 0) {
+    j = Math.floor(Math.random() * (i + 1));
+    temp = shufflearray[j];
+    shufflearray[j] = shufflearray[i];
+    shufflearray[i] = temp;
+  }
+
+  return shufflearray;
+}
+
+$(function() {
+
+  var cardsubset = shuffle(cardarray).slice(0,10);
+  var $container = $('<div>');
+  var $containerTwo = $('<div>');
+
+  cardsubset.forEach(function(element, index, array) {
+    var $img = $('<img>').attr('src', element.image);
+    debugger;
+    var $text = $('<p>' + element.reading + '</p>');
+    $container.append($img);
+    $containerTwo.append($text);
+  });
+
+  $('.card-images').append($container);
+  $('.card-readings').append($containerTwo);
+
+});
