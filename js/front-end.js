1f4e09d5e22be9278e2169e5e94b445a4856ee1e
Terri Bradley <terribradley3@gmail.com>
2016-11-09 19:19:18 -0800

    layout for large screen

--- js/front-end.js
+++ js/front-end.js
@@ -25,8 +25,12 @@ $(document).ready(function() {
   $('form').submit(function(event) {
     event.preventDefault();
     var question = $('#textBoxAlt').val();
-    $('.user-question').append('<h1>Keep in mind what you asked while reading the interpretations below:' + question + '</h1>');
+    $('.user-question').append('<h1>The readings below pertain to what was on your mind: ' + question + '</h1>');
     $('form').empty();
+    var div = $('#line');
+    div.show();
+    div.animate({height: '10px'}, 200)
+
     $('#show').show();
   });
 
@@ -54,21 +58,7 @@ $(document).ready(function() {
 
 
 
-    // var div = $("#line");
-    // var button = $("#btn");
-    // var bar = $(".bar");
-    // var input = $(".text-box ");
-    // var label = $(".text-box-label");
-    // var html = $("html, body");
-    // input.hide();
-    // bar.hide();
-    // label.hide();
-    // div.show();
-    // div.animate({height: '10px'}, 200).animate({width: '680px'}, 2000).animate({height: '450px'}, 1000);
-    // // div.animate({width: '650px'}, 800);
-    // // div.animate({rotateX: "90deg"}, "slow");
-    // html.animate({scrollTop: $(".card-images").offset().top}, 2000);
-    // button.animate({right: '-900px'}, 2000);
+
 
 
 
